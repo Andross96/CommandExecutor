adf7c321183e9ffb2cfa3967db16030dfab5c17b
André <sustac.andre@gmail.com>
2019-03-15 00:32:46 +0200

    v1.1

--- src/main/java/fr/commandexecutor/CommandExecutor.java
+++ src/main/java/fr/commandexecutor/CommandExecutor.java
@@ -2,47 +2,63 @@ package fr.commandexecutor;
 
 import java.util.List;
 
+import javax.naming.NoPermissionException;
+import javax.naming.OperationNotSupportedException;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandExecutor extends JavaPlugin {
 	public static CommandExecutor pl;
 
     @Override
-    public void onEnable() {
-    	pl = this;
-    	
-    	try {
-    		saveDefaultConfig();
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    		getServer().getPluginManager().disablePlugin(this);
-    	}
-    }
+    public void onEnable() { pl = this; saveDefaultConfig(); }
     
     @Override
     public void onDisable() {  }
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    	if(!sender.hasPermission("commandexecutor.access")) return true;
-    	getLogger().info("Player " + sender.getName() + " used command.");
-    	
-    	final List<String> list = getConfig().getStringList("command");
-    	if(list == null || list.isEmpty()) return true;
-    	final String playerName = sender.getName();
-
-    	getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-    		@Override
-			public void run() {
-    			for(String cmd : list) {
-    	    		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replace("%player%", playerName));
-    	    	}
-    		}
-    	});
-    	
+    	try {
+    		// Checking perm
+        	if(!sender.hasPermission("commandexecutor.access")) throw new NoPermissionException();
+        	
+	    	// Checking if player used valid arg
+	    	if(args.length == 0 || args[0].isEmpty()) throw new OperationNotSupportedException();
+	
+	    	// Send in console info about command used
+	    	if(getConfig().getBoolean("sendConsoleMessage")) getLogger().info("Player " + sender.getName() + " used: " + args[0]);
+	    	
+	    	// Checking for command list
+	    	ConfigurationSection cs = getConfig().getConfigurationSection("commands");
+	    	if(cs == null) throw new OperationNotSupportedException();
+	    	
+	    	// Initialise list
+	    	final List<String> list = cs.getStringList(args[0].toLowerCase());
+	    	if(list == null || list.isEmpty()) throw new OperationNotSupportedException();
+	    	
+			// Executing the command list
+	    	final String playerName = sender.getName();
+			final ConsoleCommandSender console = getServer().getConsoleSender();
+	    	getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+	    		@Override
+				public void run() {
+	    			if(!CommandExecutor.pl.isEnabled()) return;
+	    			for(String cmd : list) {
+	    	    		Bukkit.dispatchCommand(console, cmd.replace("%player%", playerName));
+	    	    	}
+	    		}
+	    	});
+    	} catch (NoPermissionException e) {
+    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("no-perm")));
+    	} catch (OperationNotSupportedException e) {
+    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("unknown-command")));
+    	}
     	
     	return true;
     }
