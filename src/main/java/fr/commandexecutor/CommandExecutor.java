26ecf7cd8cbde7475ef29e68605cfc149d3132f9
André <sustac.andre@gmail.com>
2019-04-08 22:39:39 +0300

    Update v1.3

--- src/main/java/fr/commandexecutor/CommandExecutor.java
+++ src/main/java/fr/commandexecutor/CommandExecutor.java
@@ -1,6 +1,9 @@
 package fr.commandexecutor;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.naming.NoPermissionException;
 import javax.naming.OperationNotSupportedException;
@@ -11,6 +14,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandExecutor extends JavaPlugin {
@@ -24,6 +28,7 @@ public class CommandExecutor extends JavaPlugin {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    	String commandUsage = null;
     	try {
     		// Checking perm
         	if(!sender.hasPermission("commandexecutor.access")) throw new NoPermissionException();
@@ -53,32 +58,85 @@ public class CommandExecutor extends JavaPlugin {
 	    	
 			// Executing the command list
 	    	final String playerName = sender.getName();
-			final ConsoleCommandSender console = getServer().getConsoleSender();
-	    	getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-	    		@Override
-				public void run() {
-	    			if(!CommandExecutor.pl.isEnabled()) return;
-	    			for(String cmd : list) {
-	    				if(cmd.startsWith("%sendMessage%")) {
-	    					if(sender != null) sender.sendMessage(colorize(cmd.replace("%sendMessage%", "")));
-	    					continue;
-	    				}
-	    				if(cmd.startsWith("%permission%")) {
-	    					if(sender == null) break;
-    						if(!sender.hasPermission(cmd.replace("%permission%", ""))) {
-    							sender.sendMessage(colorize(getConfig().getString("no-perm")));
-    							break;
-	    					}
-	    					continue;
-	    				}
-	    	    		Bukkit.dispatchCommand(console, cmd.replace("%player%", playerName));
-	    	    	}
-	    		}
-	    	});
+	    	CommandSender executor = getServer().getConsoleSender();
+	    	
+			for(String cmd : list) {
+				// Replacing variables
+				cmd = cmd.replace("%player%", playerName);
+				
+				// Checking the executor if executor must be the player
+				if(cmd.startsWith("%executor:")) {
+					if(cmd.contains("player")) {
+						if(!(sender instanceof Player)) {
+							sender.sendMessage(colorize("This command can only be executed by players in game."));
+							return true;
+						}
+						executor = ((Player)sender);
+					}
+					continue;
+				}
+				
+				// Checking if we need permission
+				if(cmd.startsWith("%permission%")) {
+					if(sender == null) break;
+					if(!sender.hasPermission(cmd.replace("%permission%", ""))) {
+						sender.sendMessage(colorize(getConfig().getString("no-perm")));
+						break;
+					}
+					continue;
+				}
+				
+				// Checking if a command usage is entered
+				if(cmd.startsWith("%usage%")) {
+					commandUsage = cmd.replace("%usage%", "");
+					continue;
+				}
+				
+				// Adding arguments needed in command
+				if(cmd.matches("(?i).*%arg\\d+%.*")) {
+					LinkedList<Integer> argsid = new LinkedList<Integer>();
+					final Pattern p = Pattern.compile("%arg\\d+%");
+					final Matcher m = p.matcher(cmd);
+					while (m.find()) argsid.add(Integer.parseInt(m.group(0).replaceAll("\\D+","")));
+					for(Integer arg : argsid) cmd = cmd.replace("%arg" + arg + "%", args[arg]);
+				}
+				
+				// Adding all arguments if needed
+				if(cmd.contains("%argAll")) {
+					String formatting = cmd.replaceAll("(?!%argAll\\d+%)", "");
+					final int starts = Integer.parseInt(formatting.replaceAll("\\D+", ""));
+					cmd = cmd.replace("%argAll" + starts + "%", unifyArguments(starts, args));
+				}
+				
+				// Checking if we send a message
+				if(cmd.startsWith("%sendMessage%")) {
+					sender.sendMessage(colorize(cmd.replace("%sendMessage%", "")));
+					continue;
+				}
+				
+				// Checking if we broadcast a message
+				if(cmd.startsWith("%broadcast%")) {
+					for(Player p : getServer().getOnlinePlayers()) p.sendMessage(colorize(cmd.replace("%broadcast%", "")));
+					continue;
+				}
+				
+				// Checking if the player say something
+				if(cmd.startsWith("%say%")) {
+					((Player)sender).chat(colorize(cmd.replace("%say%", "")));
+					continue;
+				}
+				
+				// Executing
+				if(executor instanceof Player) ((Player)sender).performCommand(cmd);
+				else Bukkit.dispatchCommand((ConsoleCommandSender)executor, cmd);
+			}
     	} catch (NoPermissionException e) {
     		sender.sendMessage(colorize(getConfig().getString("no-perm")));
     	} catch (OperationNotSupportedException e) {
     		sender.sendMessage(colorize(getConfig().getString("unknown-command")));
+    	} catch (Exception e) {
+    		if(commandUsage != null) sender.sendMessage(colorize(commandUsage));
+    		else sender.sendMessage(colorize(getConfig().getString("unknown-command")));
     	}
     	
     	return true;
@@ -86,6 +144,15 @@ public class CommandExecutor extends JavaPlugin {
     
     private String colorize(String message) {
     	return ChatColor.translateAlternateColorCodes('&', message);
+    }
+    
+    private String unifyArguments(int start, String[] args) {
+    	final StringBuilder message = new StringBuilder();
+		for (int i = start + 1; i < args.length; i++){
+			if (i > start + 1) message.append(" ");
+			message.append(args[i]);
+		}
+		return message.toString();
     }
 }
     
